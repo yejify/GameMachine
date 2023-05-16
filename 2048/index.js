c4c30e56c968167970db8d8d7c2f5e65e1c03ae9
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-16 23:58:10 +0900

    2048 기능 추가중(1)

--- 2048/index.js
+++ 2048/index.js
@@ -1,5 +1,7 @@
 const board = document.querySelector(".game-container");
 const cells = [];
+const numArr = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+let score = 0;
 for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 4; j++) {
     const cell = document.createElement("div");
@@ -8,7 +10,16 @@ for (let i = 0; i < 4; i++) {
     board.appendChild(cell);
   }
 }
-// console.log(cells);
+
+// 게임 초기화
+function gameInit() {
+  for (let i = 0; i < 16; i++) {
+    cells[i].textContent = "";
+    numArr[i] = 0;
+  }
+  const score = document.querySelector(".now .score-value");
+  score.textContent = 0;
+}
 // 타일 생성
 function randomTile() {
   const tiles = document.querySelectorAll(".tile");
@@ -48,23 +59,21 @@ document.addEventListener("keydown", (event) => {
 // 타일 이동
 const moveTile = (direction) => {
   const tileElements = document.querySelectorAll(".tile");
-
+  // console.log(tileElements);
   // 위로 이동
   if (direction === "up") {
     console.log("실행됨");
     for (let i = 4; i < tileElements.length; i++) {
       const tile = tileElements[i];
       const topTile = tileElements[i - 4];
-      // if (!topTile) continue;
-
-      if (!topTile.classList.contains("tile")) {
-        console.log("빈타일이였음");
-        topTile.appendChild(tile);
-        i -= 5;
-      } else if (topTile.classList.contains(tile.classList[1])) {
-        /**
-         *  !같은 숫자인 경우에만 합쳐지는 조건 추가해야함
-         * */
+      const topTilehasOtherClasses = [...topTile.classList].some(
+        (cls) => cls !== "tile"
+      );
+      const tilehasOtherClasses = [...tile.classList].some(
+        (cls) => cls !== "tile"
+      );
+      // topTile과 tile이 같은 숫자인 경우 합쳐줌
+      if (topTile.classList.contains(tile.classList[1])) {
         console.log("합쳐짐");
         const tileValue = topTile.classList[1].replace(/\D/g, "");
         const newValue = tileValue * 2;
@@ -73,6 +82,20 @@ const moveTile = (direction) => {
         topTile.textContent = `${newValue}`;
         tile.classList.remove(tile.classList[1]);
         tile.textContent = "";
+        scoreCnt += newValue;
+        scoreNow(scoreCnt);
+      }
+      // topTile이 빈타일인경우
+      if (!topTilehasOtherClasses) {
+        console.log("빈타일이였음");
+        topTile.textContent = tile.textContent;
+        topTile.classList.remove(topTile.classList[1]);
+        if (tilehasOtherClasses) {
+          topTile.classList.add(tile.classList[1]);
+        }
+        tile.textContent = "";
+        tile.classList.remove(tile.classList[1]);
+        // i -= 5;
       }
     }
   }
@@ -88,83 +111,88 @@ const moveTile = (direction) => {
         bottomTile.appendChild(tile);
         i += 5;
       } else if (bottomTile.classList.contains(tile.classList[1])) {
-        bottomTile.classList.remove(tile.classList[1]);
-        const tileValue = parseInt(bottomTile.classList[1]);
+        const tileValue = bottomTile.classList[1].replace(/\D/g, "");
         const newValue = tileValue * 2;
+        bottomTile.classList.remove(tile.classList[1]);
         bottomTile.classList.add(`tile-${newValue}`);
+        bottomTile.textContent = `${newValue}`;
+        tile.classList.remove(tile.classList[1]);
+        tile.textContent = "";
+      }
+    }
+  }
+  // 왼쪽으로 이동
+  if (direction === "left") {
+    for (let i = 1; i < tileElements.length; i++) {
+      const tile = tileElements[i];
+      const leftTile = tileElements[i - 1];
+      if (i % 4 === 0) continue;
+
+      if (!leftTile.classList.contains("tile")) {
+        leftTile.appendChild(tile);
+        i -= 2;
+      } else if (leftTile.classList.contains(tile.classList[1])) {
+        leftTile.classList.remove(tile.classList[1]);
+        const tileValue = leftTile.classList[1].replace(/\D/g, "");
+        const newValue = tileValue * 2;
+        leftTile.classList.add(`tile-${newValue}`);
         tile.remove();
       }
     }
-    // 왼쪽으로 이동
-    if (direction === "left") {
-      for (let i = 1; i < tileElements.length; i++) {
-        const tile = tileElements[i];
-        const leftTile = tileElements[i - 1];
-        if (i % 4 === 0) continue;
-
-        if (!leftTile.classList.contains("tile")) {
-          leftTile.appendChild(tile);
-          i -= 2;
-        } else if (leftTile.classList.contains(tile.classList[1])) {
-          leftTile.classList.remove(tile.classList[1]);
-          const tileValue = parseInt(leftTile.classList[1]);
-          const newValue = tileValue * 2;
-          leftTile.classList.add(`tile-${newValue}`);
-          tile.remove();
-        }
-      }
-      // 오른쪽으로 이동
-      if (direction === "right") {
-        for (let i = tileElements.length - 2; i >= 0; i--) {
-          const tile = tileElements[i];
-          const rightTile = tileElements[i + 1];
-          if ((i + 1) % 4 === 0) continue;
-
-          if (!rightTile.classList.contains("tile")) {
-            rightTile.appendChild(tile);
-            i += 2;
-          } else if (rightTile.classList.contains(tile.classList[1])) {
-            rightTile.classList.remove(tile.classList[1]);
-            const tileValue = parseInt(rightTile.classList[1]);
-            const newValue = tileValue * 2;
-            rightTile.classList.add(`tile-${newValue}`);
-            tile.remove();
-          }
-        }
-      }
-
-      randomTile();
-    }
-
-    //     // 게임 종료 체크
-    //     const checkGameOver = () => {
-    //       const tileElements = document.querySelectorAll(".tile");
-
-    //       for (let i = 0; i < tileElements.length; i++) {
-    //         const tile = tileElements[i];
-    //         if (
-    //           tile.classList.contains("tile-2048") ||
-    //           (i % 4 !== 3 &&
-    //             tile.classList.contains(tileElements[i + 1].classList[1])) ||
-    //           (i < 12 && tile.classList.contains(tileElements[i + 4].classList[1]))
-    //         ) {
-    //           return false;
-    //         }
-    //       }
-
-    //       return true;
-    //     };
-
-    //     // 게임 종료
-    //     const endGame = () => {
-    //       alert("게임 종료!");
-    //     };
-
-    //     // 게임 상태 체크
-    //     setInterval(() => {
-    //       if (checkGameOver()) {
-    //         endGame();
-    //       }
-    //     }, 1000);
   }
+  // 오른쪽으로 이동
+  if (direction === "right") {
+    for (let i = tileElements.length - 2; i >= 0; i--) {
+      const tile = tileElements[i];
+      const rightTile = tileElements[i + 1];
+      if ((i + 1) % 4 === 0) continue;
+
+      if (!rightTile.classList.contains("tile")) {
+        rightTile.appendChild(tile);
+        i += 2;
+      } else if (rightTile.classList.contains(tile.classList[1])) {
+        rightTile.classList.remove(tile.classList[1]);
+        const tileValue = rightTile.classList[1].replace(/\D/g, "");
+        const newValue = tileValue * 2;
+        rightTile.classList.add(`tile-${newValue}`);
+        tile.remove();
+      }
+    }
+  }
+
+  //     // 게임 종료 체크
+  //     const checkGameOver = () => {
+  //       const tileElements = document.querySelectorAll(".tile");
+
+  //       for (let i = 0; i < tileElements.length; i++) {
+  //         const tile = tileElements[i];
+  //         if (
+  //           tile.classList.contains("tile-2048") ||
+  //           (i % 4 !== 3 &&
+  //             tile.classList.contains(tileElements[i + 1].classList[1])) ||
+  //           (i < 12 && tile.classList.contains(tileElements[i + 4].classList[1]))
+  //         ) {
+  //           return false;
+  //         }
+  //       }
+
+  //       return true;
+  //     };
+
+  //     // 게임 종료
+  //     const endGame = () => {
+  //       alert("게임 종료!");
+  //     };
+
+  //     // 게임 상태 체크
+  //     setInterval(() => {
+  //       if (checkGameOver()) {
+  //         endGame();
+  //       }
+  //     }, 1000);
+  randomTile();
 };
+// const scoreNow = (scoreCnt) => {
+//   const score = document.querySelector(".now .score-value");
+//   score.textContent = scoreCnt;
+// };
