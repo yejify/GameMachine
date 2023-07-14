21bf090ee9cea830c380c74536cfe7b1689c3463
yeji <skql4128@naver.com>
2023-07-14 23:53:09 +0900

    두더지 못잡았을 때 이벤트 로직 추가

--- Whack_a_mole/index.js
+++ Whack_a_mole/index.js
@@ -12,24 +12,24 @@
 // mole.setAttribute("src","../assets/mole.PNG");
 
 //랜덤으로 두더지 나오는 함수
-function randomCreateMole(){
-    const liList = document.querySelectorAll("div");
-    const img = document.createElement("img");
-    const mole = liList.item(Math.floor(Math.random() * 9)).appendChild(img)
+function randomCreateMole() {
+  const liList = document.querySelectorAll('div');
+  const img = document.createElement('img');
+  const mole = liList.item(Math.floor(Math.random() * 9)).appendChild(img);
 
-    //두더지 생성
-    mole.setAttribute("id","mole");
-    mole.setAttribute("src","../assets/mole.PNG");
+  //두더지 생성
+  mole.setAttribute('id', 'mole');
+  mole.setAttribute('src', '../assets/mole.PNG');
 
-    // //3초뒤 두더지 제거
-    // setTimeout(removeMole, 3000);
-    // randomCreateMole();
+  // //3초뒤 두더지 제거
+  // setTimeout(removeMole, 3000);
+  // randomCreateMole();
 }
 
 //두더지 제거 함수
-function removeMole(){
-    const dieMole = document.getElementById('mole');
-    dieMole.remove();
+function removeMole() {
+  const dieMole = document.getElementById('mole');
+  dieMole.remove();
 }
 
 //점수
@@ -37,79 +37,92 @@ let score = 0;
 // let clickCount = 0;
 
 //start 버튼 클릭 이벤트에 두더지 함수 실행
-function startBtnClick(){
-    randomCreateMole();
+function startBtnClick() {
+  randomCreateMole();
 
-    const startBtn = document.querySelector("#startBtn");
-    startBtn.classList.add("a11y-hidden");
+  const startBtn = document.querySelector('#startBtn');
+  startBtn.classList.add('a11y-hidden');
 
-    //이미지에 커서 포인터 css적용
-    const selectImg = document.querySelectorAll("img");
-    //위 이미지 각각에 전부 class 줘야 하는데 뭘 써야 하더라..
+  //이미지에 커서 포인터 css적용
+  const selectImg = document.querySelectorAll('img');
+  //위 이미지 각각에 전부 class 줘야 하는데 뭘 써야 하더라..
 }
 
 //게임 스타트 버튼 클릭 이벤트
-document.getElementById("startBtn").addEventListener("click", startBtnClick);
+document.getElementById('startBtn').addEventListener('click', startBtnClick);
 
 //두더지 클릭하면 사라짐, 새로운 두더지 등장
-function catchMole(e){
-    // clickCount++
-    //클릭한 요소 가져오기
-    const clickTaget = e.target.getAttribute("id");
-    
-    //li에 두더지 이미지 있는지 확인
-    if(clickTaget === 'mole'){
+function catchMole(e) {
+  // clickCount++
+  //클릭한 요소 가져오기
+  const clickTaget = e.target.getAttribute('id');
+
+  //li에 두더지 이미지 있는지 확인
+  if (clickTaget === 'mole') {
     //있으면 점수 +1
-    score++
+    score++;
     //현재 두더지 이미지 삭제
     removeMole();
     randomCreateMole();
-    }else{
+  } else {
     //현재 두더지 이미지 삭제
-    removeMole()
-        //결과화면
-        const ground = document.querySelector('#ground');
-        const gameResult = document.querySelector('#gameResult');
-        ground.classList.add("a11y-hidden");
-        gameResult.classList.remove("a11y-hidden");
-    
-        //score 보여주기
-        const resultScore = document.querySelector('#resultScore');
-        resultScore.textContent=score*10;
-    };
+    removeMole();
+    //결과화면
+    const ground = document.querySelector('#ground');
+    const gameResult = document.querySelector('#gameResult');
+    ground.classList.add('a11y-hidden');
+    gameResult.classList.remove('a11y-hidden');
 
-    // //클릭 감지, 10번이면 결과페이지로 넘어가기
-    // if(clickCount === 10){
+    //score 보여주기
+    const resultScore = document.querySelector('#resultScore');
+    resultScore.textContent = score * 10;
+  }
 
-    //     //li에 두더지 이미지 있는지 확인
-    //     if(clickTaget === 'mole'){
-    //     //있으면 점수 +1
-    //     score++
-    //     //현재 두더지 이미지 삭제
-    //     removeMole()
-    //     }else{
-    //     //현재 두더지 이미지 삭제
-    //     removeMole()
-    //     }
+  // //클릭 감지, 10번이면 결과페이지로 넘어가기
+  // if(clickCount === 10){
 
-        // //결과화면
-        // const ground = document.querySelector('#ground');
-        // const gameResult = document.querySelector('#gameResult');
-        // ground.classList.add("a11y-hidden");
-        // gameResult.classList.remove("a11y-hidden");
+  //     //li에 두더지 이미지 있는지 확인
+  //     if(clickTaget === 'mole'){
+  //     //있으면 점수 +1
+  //     score++
+  //     //현재 두더지 이미지 삭제
+  //     removeMole()
+  //     }else{
+  //     //현재 두더지 이미지 삭제
+  //     removeMole()
+  //     }
 
-        // //score 보여주기
-        // const resultScore = document.querySelector('#resultScore');
-        // resultScore.textContent=score*10;
+  // //결과화면
+  // const ground = document.querySelector('#ground');
+  // const gameResult = document.querySelector('#gameResult');
+  // ground.classList.add("a11y-hidden");
+  // gameResult.classList.remove("a11y-hidden");
 
-    };
+  // //score 보여주기
+  // const resultScore = document.querySelector('#resultScore');
+  // resultScore.textContent=score*10;
+}
 
-function reloadGame(){
-    location.reload();
+function reloadGame() {
+  location.reload();
+}
+
+//두더지를 못잡았을 때
+function catchMole(e) {
+  const clickTarget = e.target.getAttribute('id');
+
+  if (clickTarget === 'mole') {
+    score++;
+    removeMole();
+  } else {
+    removeMole();
+  }
+
+  randomCreateMole();
 }
 
 //두더지 잡기 클릭이벤트
-document.querySelector("ul").addEventListener("click", catchMole);
+document.querySelector('ul').addEventListener('click', catchMole);
 
 //restart 버튼 클릭이벤트
-document.querySelector("#restartBtn").addEventListener("click", reloadGame);
+document.querySelector('#restartBtn').addEventListener('click', reloadGame);
